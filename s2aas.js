263e500fe1f7cf51908ccf52b2ac1bb7eb2e9018
Wörner  Dominic <dwoerner@ethz.ch>
2014-05-09 13:51:03 +0200

    Parse unconfirmed transaction to sensor address by using BitEasy web sockets api

---
+++ s2aas.js
@@ -0,0 +1,57 @@
+
+var btcAddress = 'mxjr2jvmbNFjQHA8qQ7FkE7jCX7E1jqWrK'
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+// Require websocket client. Later used to get instant payment notification.
+var WebSocketClient = require('websocket').client;
+
+
+
+
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    console.log('WebSocket client connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function(message) {
+    	var message = message.utf8Data;
+    	var message = message.toString();
+    	var message = message.replace(/\\/g, '');
+    	var message = JSON.parse(message);
+        console.log('Received message of typ: '+message.event );
+        if (message.event == 'transactions:create') {
+            var sender = message.data.inputs[0].from_address;
+            var data = message.data.outputs;
+            for(var i = 0; i<data.length; i++) {
+            	if (data[i].to_address == btcAddress) {
+            		console.log('Received '+data[i].value+' satoshis from '+sender)
+            	}
+			}
+        }
+    });
+
+    function requestPaymentNotification(btcAddress) {
+        if (connection.connected) {
+            var json = {
+    			"event": "transactions:create",
+    			"filters": {
+        			"addresses": [btcAddress],
+        			"confidence": "UNCONFIRMED"
+    			}
+			}
+            connection.sendUTF(JSON.stringify(json));
+            //setTimeout(sendNumber, 1000);
+        }
+    }
+    requestPaymentNotification(btcAddress);
+});
+
+client.connect('wss://ws.biteasy.com/testnet/v1');
